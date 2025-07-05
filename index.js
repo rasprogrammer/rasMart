a767350ff53f6405a5b0c4b61f27343eedf9e175
rasprogrammer <rasprogrammer@gmail.com>
2025-07-05 18:44:31 +0530

    api authentication completed

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const dbURL = process.env.DB_URL;
 connectDB(dbURL);
 
 const authRoutes = require("./routes/authRoutes");
-const { authMiddleware } = require("./middlewares/authMiddleware");
+const { authMiddleware, authRoles } = require("./middlewares/authMiddleware");
 
 app.get("/", (req, res) => {
     return res.end("<h1 style='text-align:center;'>E-commerce with Rajiv<h1>");
@@ -27,5 +27,8 @@ app.get("/api/profile", authMiddleware, (req, res) => {
     return res.end(`Profile user name : ${req.user.name}`);
 });
 
+app.get("/api/admin", authMiddleware, authRoles("admin"), (req, res) => {
+    return res.end(`Admin user`);
+})
 
 app.listen(PORT, () => console.log("Server started"));
--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -21,9 +21,20 @@ const authMiddleware = async (req, res, next) => {
 };
 
 const authRoles = (...roles) => {
+    return (req, res, next) => {
+        if (!req.user) {
+            return res.status(401).json({ message: "Unauthorized: User not found" });
+        }
 
+        if (!roles.includes(req.user.role)) {
+            return res.status(403).json({ message: "Access denied" });
+        }
+
+        next();
+    };
 }
 
 module.exports = {
-    authMiddleware
+    authMiddleware,
+    authRoles
 };
